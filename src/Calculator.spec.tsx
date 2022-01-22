5d0d06554761330d98dd2748b34d1783d9b98812
Dan McGrath <daniel.mcgrath9@gmail.com>
2022-01-22 10:58:44 -0500

    chore: tests

--- src/Calculator.spec.tsx
+++ src/Calculator.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { Calculator } from "./Calculator";
 
 describe("Calculator", () => {
@@ -8,5 +8,24 @@ describe("Calculator", () => {
     expect(screen.getByText("Time")).toBeInTheDocument();
     expect(screen.getByText("Pace")).toBeInTheDocument();
     expect(screen.getByText("Distance")).toBeInTheDocument();
+  });
+
+  it("calculates time", async () => {
+    render(<Calculator />);
+
+    fireEvent.change(screen.getByPlaceholderText("Distance"), {
+      target: {
+        value: 10,
+      },
+    });
+    fireEvent.change(screen.getAllByPlaceholderText("Min")[1], {
+      target: {
+        value: 5,
+      },
+    });
+    fireEvent.click(screen.getAllByText("Calculate")[0]);
+    await waitFor(() => {
+      expect(screen.getAllByPlaceholderText("Min")[0]).toHaveValue(50);
+    });
   });
 });

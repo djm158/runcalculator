667699c1e86376188a382beb08e5673b12c6efc2
Dan McGrath <daniel.mcgrath9@gmail.com>
2024-12-19 05:36:07 -0500

    chore: update react testing library, rename test files

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { App } from "./App";
+import { render } from "@testing-library/react";
 
 it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  expect(render(<App />)).toBeTruthy();
 });

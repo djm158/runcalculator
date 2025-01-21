5159bdd055cd6f836860ec81ff4c74bef8937244
Dan McGrath <daniel.mcgrath9@gmail.com>
2025-01-21 10:37:42 -0500

    feat: remove theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,7 @@
 import "./styles.css";
 
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-
 import { Calculator } from "./components/Calculator";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#5178fc",
-      light: "#7393FC",
-      dark: "#3854B0",
-      contrastText: "#fff",
-    },
-    secondary: {
-      main: "#f50057",
-      light: "#F73378",
-      dark: "#AB003C",
-      contrastText: "#fff",
-    },
-  },
-  components: {
-    MuiInputBase: {
-      styleOverrides: {
-        input: {
-          color: "#292d3a",
-          backgroundColor: "#fffcfc",
-          borderRadius: "4px",
-        },
-      },
-    },
-  },
-});
-
 export const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <Calculator />
-    </ThemeProvider>
-  );
+  return <Calculator />;
 };

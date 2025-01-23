9dcd06e032eb3648babce97520ca208ce099b8e5
Dan McGrath <daniel.mcgrath9@gmail.com>
2025-01-22 20:21:16 -0500

    feat: add theme provider, gradient background

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { Calculator } from "./components/Calculator";
+import { ThemeProvider } from "./components/theme-provider";
 
 import "./styles.css";
 
 export const App = () => {
-  return <Calculator />;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="min-h-screen bg-gradient-to-br dark:from-pink-900 dark:to-blue-900 text-foreground">
+        <Calculator />;
+      </div>
+    </ThemeProvider>
+  );
 };
